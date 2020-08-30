107d09edfe5361029aa6c04aab33ec833758a6c9
aikrom <undercode.dev@gmail.com>
2020-08-30 13:53:44 +0700

    moved index.css to styles folder and added App styles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { useAuth } from './hooks';
+import './styles/index.css';
 
 export const App = () => {
 	const [auth, authError] = useAuth();
 
 	if (authError) {
-		return <div>Auth error</div>;
+		return <div className="app">Auth error</div>;
 	}
 
-	return <div className="App">{auth ? auth.uid : 'Create authentificate connection...'}</div>;
+	if (!auth) {
+		return <div className="app">Create authentificate connection...</div>;
+	}
+
+	return <div className="app">App</div>;
 };
 
 export default App;
